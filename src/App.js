6663a0d6d6d5652e4fa90054c11283e540bbb3d8
Shailesh <shaileshruns1995@gmail.com>
2024-07-20 23:29:54 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,40 +1,43 @@
 
 import { useEffect, useState } from 'react';
 import './App.css';
+import Calcy from './calcy';
 
 function App() {
 
    const [user,setuser] = useState("");
    const [gusers,setguser] = useState("");
 
-   useEffect(()=>{
-       async function gituser(){
-        const gituser=await fetch(`https://api.github.com/users`);
-        const auser =await gituser.json();
-        setguser(auser);
-        console.log(auser);
+  //  useEffect(()=>{
+  //      async function gituser(){
+  //       const gituser=await fetch(`https://api.github.com/users`);
+  //       const auser =await gituser.json();
+  //       setguser(auser);
+  //       console.log(auser);
 
-       }
-       gituser();
-   },[user])
+  //      }
+  //      gituser();
+  //  },[user])
 
  //here important thing is that how avoiding event.target.value only object destructuring****************
  //**************************************************************************************/
- const handleinp = ({target:{value}})=>{
-  
-       setuser(value);
-       console.log(value);
- }
+//  const handleinp = ({target:{value}})=>{
+//        setuser(value);
+//        console.log(value);
+//      }
+
 
   return (
     <div className="App">
       <h1>{user}</h1>
       <p>{gusers.length}</p>
-      {
+      {/* {
         gusers.map(elm=> <img src={elm.avatar_url} width="250px"/>)
-      }
+      } */}
      
-      <input  type='text' value={user} onInput={handleinp}/>
+      {/* <input  type='text' value={user} onInput={handleinp}/> */}
+
+      <Calcy />
     </div>
   );
 
---
+++ src/calcy.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+ let operator =  [1,2,3,4,5,6,7,8,9,0,'.','+','-','*','/','=','c'];
+ 
+const Calcy = ()=>{
+   const [state,setstate] =useState("");
+
+     function handlecal({target:{innerText}}){
+            
+        if(innerText=='='){
+            document.getElementById('display').innerText=eval(state);
+            setstate(`${eval(state)}`);
+        } else if(innerText=='c'){
+            setstate(prev=>{
+                document.getElementById('display').innerText=prev.slice(0,prev.length-1);
+                return prev.slice(0,prev.length-1);
+            });
+        } 
+        else{
+            setstate(prev=>{
+                document.getElementById('display').innerText=prev+innerText;
+                return prev+innerText
+            });
+        }
+           
+            
+     }
+    return(
+    <div className="calcy">
+       <div className="display" id="display"></div>
+       <div className="opertor">
+         {
+            operator.map((elm)=> <div onClick={handlecal}>{elm}</div>)
+         }
+       </div>
+    </div>
+    )
+}
+export default Calcy;
